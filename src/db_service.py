aa91adb358a301a2da6ee32d394f173be01c7e6e
Tomasz Grodecki <tgrodecki89@gmail.com>
2026-01-15 02:51:00 +0100

    if name=main block and main function moved to playground folder

--- src/db_service.py
+++ src/db_service.py
@@ -151,32 +151,3 @@ class DatabaseService:
     @staticmethod
     def prepare_values(data, columns):
         return [tuple(row.get(col, 0) for col in columns) for row in data]
-
-
-def main():
-    from config import Config
-    db = DatabaseService(Config())
-    db.connect()
-
-    try:
-        db.execute("DROP TABLE IF EXISTS routes_costs, monthly_costs, clients CASCADE")
-        db.create_tables()
-
-        db.load_json("data/monthly_costs.json",
-                     "monthly_costs",
-                     ["cost_name", "amount"])
-
-        db.load_json("data/routes_costs.json",
-                     "routes_costs",
-                     ["route_name", "fuel", "tolls", "ferry", "hotel"])
-
-        db.load_json("data/clients.json",
-                     "clients",
-                     ["client_name", "client_class", "avg_payment_delay_days",
-                              "late_payment_count", "total_shipments"])
-    finally:
-        db.disconnect()
-
-
-if __name__ == "__main__":
-    main()
