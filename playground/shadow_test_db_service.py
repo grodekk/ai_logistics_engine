d9c45b5421d37e6d23a6feb41c594ddb17d219d2
Tomasz Grodecki <tgrodecki89@gmail.com>
2026-02-03 03:54:13 +0100

    Refactor: Rename method to 'insert_json_into_table'

--- playground/shadow_test_db_service.py
+++ playground/shadow_test_db_service.py
@@ -9,15 +9,15 @@ try:
     db.execute("DROP TABLE IF EXISTS routes_costs, monthly_costs, clients CASCADE")
     db.create_tables()
 
-    db.load_json("data/monthly_costs.json",
+    db.insert_json_into_table("monthly_costs.json",
                  "monthly_costs",
                  ["cost_name", "amount"])
 
-    db.load_json("data/routes_costs.json",
+    db.insert_json_into_table("routes_costs.json",
                  "routes_costs",
                  ["route_name", "fuel", "tolls", "ferry", "hotel"])
 
-    db.load_json("data/clients.json",
+    db.insert_json_into_table("clients.json",
                  "clients",
                  ["client_name", "client_class", "avg_payment_delay_days",
                           "late_payment_count", "total_shipments"])
