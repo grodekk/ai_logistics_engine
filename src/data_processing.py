b8a6e7dc47fcc66ad8ec8b1e76acb9a4d02f2f26
Tomasz Grodecki <tgrodecki89@gmail.com>
2026-02-25 22:59:25 +0100

    refactor: use COALESCE and add total costs in SQL queries

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+
 class DataProcessing:
     def __init__(self, db_service):
         self.db = db_service
+
 
     def get_monthly_costs(self):
         query = """
@@ -13,22 +15,29 @@ class DataProcessing:
         df = pd.DataFrame(rows, columns=["cost_name", "amount"])
         return self._cast_columns_to_float(df, ["amount"])
 
+
     def get_routes_costs(self):
         query = """
-        SELECT route_name, fuel, tolls, ferry, hotel
-        FROM routes_costs
+        SELECT route_name,
+               COALESCE(fuel, 0) AS fuel,
+               COALESCE(tolls, 0) AS tolls,
+               COALESCE(ferry, 0) AS ferry,
+               COALESCE(hotel, 0) AS hotel,
+               COALESCE(fuel, 0) + COALESCE(tolls, 0) + COALESCE(ferry, 0) + COALESCE(hotel, 0) AS total_route_cost
+        FROM routes_costs;
         """
         rows = self.db.fetch_all(query)
-        df = pd.DataFrame(rows, columns=["route_name", "fuel", "tolls", "ferry", "hotel"])
-        return self._cast_columns_to_float(df, ["fuel", "tolls", "ferry", "hotel"])
+        df = pd.DataFrame(rows, columns=["route_name", "fuel", "tolls", "ferry", "hotel", "total_route_cost"])
+        return self._cast_columns_to_float(df, ["fuel", "tolls", "ferry", "hotel", "total_route_cost"])
+
 
     def get_clients(self):
         query = """
         SELECT client_name,
-               avg_payment_delay_days,
-               late_payment_count,
                client_class,
-               total_shipments
+               COALESCE(avg_payment_delay_days, 0) AS avg_payment_delay_days,
+               COALESCE(late_payment_count, 0) AS late_payment_count,               
+               COALESCE(total_shipments, 0) AS total_shipments
         FROM clients
         """
         rows = self.db.fetch_all(query)
@@ -42,6 +51,7 @@ class DataProcessing:
                                               "total_shipments"
         ])
         return df
+
 
     @staticmethod
     def _cast_columns_to_float(df, columns):
