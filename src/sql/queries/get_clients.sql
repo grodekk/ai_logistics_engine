d8595199e9647a52abf19ec0931cc3b57f8198e6
Tomasz Grodecki <tgrodecki89@gmail.com>
2026-03-25 22:05:17 +0100

    refactor(sql): removed coalesce

--- src/sql/queries/get_clients.sql
+++ src/sql/queries/get_clients.sql
@@ -1,6 +1,5 @@
 SELECT client_name,
     client_class,
-    COALESCE(avg_payment_delay_days, 0) AS avg_payment_delay_days,
-    COALESCE(late_payment_count, 0) AS late_payment_count,
-    COALESCE(total_shipments, 0) AS total_shipments
+    avg_payment_delay_days,
+    late_payment_count,
 FROM clients
--- src/sql/queries/get_routes_costs.sql
+++ src/sql/queries/get_routes_costs.sql
@@ -1,7 +1,7 @@
 SELECT route_name,
-    COALESCE(fuel, 0) AS fuel,
-    COALESCE(tolls, 0) AS tolls,
-    COALESCE(ferry, 0) AS ferry,
-    COALESCE(hotel, 0) AS hotel,
-    COALESCE(fuel, 0) + COALESCE(tolls, 0) + COALESCE(ferry, 0) + COALESCE(hotel, 0) AS total_route_cost
+    fuel,
+    tolls,
+    ferry,
+    hotel,
+    fuel + tolls + ferry + hotel AS total_route_cost
 FROM routes_costs;
